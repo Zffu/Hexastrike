d6c0126cd7d7ca63a5b42ab0d95c4ca62662e2c5
Zffu <radi0o0@outlook.com>
2025-03-02 17:27:48 +0100

    feat: added r_handler

--- include/server.h
+++ include/server.h
@@ -1,6 +1,8 @@
 #include <hexastrike.h>
 #include <thread/pool.h>
 
+#include <client/connection.h>
+
 #ifndef SERVER_H
 #define SERVER_H
 
@@ -14,6 +16,8 @@ typedef struct HEXASTRIKE_SOCKET_SERVER {
     socket_t server_socket;
     IO_THREAD_POOL pool;
 
+    void (*r_handler)(CONNECTION, unsigned char*, int);
+
 } HEXASTRIKE_SERVER;
 
 HEXASTRIKE_SERVER* hexastrike_sinit(short port);
@@ -21,8 +25,6 @@ HEXASTRIKE_SERVER* hexastrike_sinit(short port);
 void hexastrike_sdown(HEXASTRIKE_SERVER* server);
 
 void hexastrike_dloop(HEXASTRIKE_SERVER* server);
-
-
 void hexastrike_iopinit(HEXASTRIKE_SERVER* server);
 
 #endif
