8d69a7dd33abf27beee53a7dd159407a1207587a
Zffu <radi0o0@outlook.com>
2025-03-07 17:45:19 +0100

    feat: added hexastrike_dloop_thread_exec references in thread header

--- include/thread/thread.h
+++ include/thread/thread.h
@@ -9,6 +9,14 @@ unsigned __stdcall hexastrike_io_thread_pool_member_exec(void* arg); // HEXASTIR
 void* hexastrike_io_thread_pool_member_exec(void* arg); // HEXASTIRKE_IOPOOL_MEMBER_EXECCTX
 #endif
 
+#if defined(_WIN32) & defined (HEXASTRIKE_DLOOP_THREADED)
+unsigned __stdcall hexastrike_dloop_thread_exec(void* arg); // HEXASTRIKE_SERVER
+#endif
+
+#if !defined(_WIN32) & defined (HEXASTRIKE_DLOOP_THREADED)
+void* hexastrike_dloop_thread_exec(void* arg); // HEXASTRIKE_SERVER
+#endif
+
 void thread_sleep(long time);
 
 #endif
